d8210ca9b7e6d851a917c90055c76ea4024ddedb
Irham Dumidae <irham39986@gmail.com>
2025-06-26 14:43:20 +0300

    Refactor ProductLists component to include new product images and update product data structure
    - Added SVG assets for baking, bread, cake, cookie cream, and cookie.
    - Updated ProductLists component to utilize new product images.
    - Modified product data structure to include image paths for each product.
    - Enhanced rendering logic to display product images alongside names.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,8 @@
+// rafce -- React Arrow Function Component Export
+
 import Header from "./components/Header.jsx";
 import ProductLists from "./components/ProductLists.jsx";
 import Products from "./components/Products.jsx";
-import "./App.css";
 import Footer from "./components/Footer.jsx";
 
 function App() {
--- my-app/src/components/ProductLists.jsx
+++ my-app/src/components/ProductLists.jsx
@@ -1,12 +1,18 @@
+import baking from "../assets/baking.svg";
+import bread from "../assets/bread.svg";
+import cake from "../assets/cake.svg";
+import cookieCream from "../assets/cookie-cream.svg";
+import cookie from "../assets/cookie.svg";
+
 import { useState } from "react";
 
 function ProductLists() {
   const [data, setData] = useState([
-    { id: 1, name: "Croissant" },
-    { id: 2, name: "Baguette" },
-    { id: 3, name: "Chocolate" },
-    { id: 3, name: "Cinnamon" },
-    { id: 3, name: "Blueberry Muffin" },
+    { id: 1, name: "baking", image: baking },
+    { id: 2, name: "bread", image: bread },
+    { id: 3, name: "cake", image: cake },
+    { id: 4, name: "cookie-cream", image: cookieCream },
+    { id: 5, name: "cookie", image: cookie },
   ]);
   const [show, setShow] = useState(true);
   return (
@@ -14,7 +20,15 @@ function ProductLists() {
       <h1>Totall Products : {data.length}</h1>
       <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
       <ol>
-        {show && data.map((person) => <li key={person.id}>{person.name}</li>)}
+        {show &&
+          data.map((Product) => (
+            <li>
+              {Product.image && (
+                <img src={Product.image} alt={Product.name} width="25" />
+              )}
+              {Product.name}
+            </li>
+          ))}
       </ol>
     </div>
   );
