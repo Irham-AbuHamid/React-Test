4773db182b391fc49c62e742d5beee868cc46231
Irham Dumidae <irham39986@gmail.com>
2025-06-26 11:22:26 +0300

    Initialize my-app with React and Vite setup
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added Vite logo SVG to the public directory.
    - Created App component with state management for displaying a list of people.
    - Added React logo SVG to assets directory.
    - Created App.css and index.css for styling.
    - Set up main entry point (main.jsx) for rendering the App component.
    - Configured Vite with a basic configuration file (vite.config.js).

---
+++ my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState([
+    { id: 1, name: "AbuHamid", gender: "Man" },
+    { id: 2, name: "Farah", gender: "Girl" },
+    { id: 3, name: "Sir", gender: "Man" },
+  ]);
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <h1>Totall People : {data.length}</h1>
+      <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      <ol>
+        {show &&
+          data.map((person) => <li key={person.id}>Name: {person.name}</li>)}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
